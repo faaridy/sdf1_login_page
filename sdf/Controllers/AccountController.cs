d79b5c53aae6adf03c2ed7f46b82e9bb50b23043
faaridy <faaridy9@gmail.com>
2024-04-06 19:59:18 +0400

    Add project files.

---
+++ sdf/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using System.Web.Mvc;
+using sdf.Models;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace sdf.Controllers
+{
+    public class AccountController : Controller
+    {
+        MySqlConnection connection = new MySqlConnection();
+        MySqlCommand command = new MySqlCommand();
+        MySqlDataReader reader;
+        // GET: Account
+        [HttpGet]
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+        void connectionString()
+        {
+            connection.ConnectionString = "server=localhost;uid=root;" +
+    "pwd=12345;database=login";
+        }
+        [HttpPost]
+        public ActionResult Verify(Account acc)
+        {
+            connectionString();
+            connection.Open();
+            command.Connection = connection;
+            command.CommandText = "select * from login where username = '"+acc.Name+"' and password = '"+acc.Password+"'";
+            reader = command.ExecuteReader();
+            if (reader.Read())
+            {
+                connection.Close();
+                return View("Create");
+            }
+            else
+            {
+                connection.Close();
+                return View("Error");
+            }
+            
+            
+    }
+    }
+    
+}
